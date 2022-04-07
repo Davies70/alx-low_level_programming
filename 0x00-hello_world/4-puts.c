eeb21047a216ba0a99e7ba8479b8d91d15be9468
Ajayi <ajayidavies7@gmail.com>
2022-04-07 16:46:54 -0700

    P

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,7 +1,9 @@
 #include <stdio.h>
+
 int main(void)
-{ 
-	char *x = " \"Programming is like building a multilingual puzzle";
+{
+	char *x = "\"Programming is like building a multilingual puzzle";
+
 	puts(x);
-    return (0);
+		return (0);
 }
