6f0be6dfefa9e7af19924b9fb9ec5449e7556e7c
Ajayi <ajayidavies7@gmail.com>
2022-04-13 08:30:19 -0700

    5

---
+++ 0x02-functions_nested_loops/5-sign.c
@@ -0,0 +1,30 @@
+#include "main.h"
+
+/**
+ * print_sign - Short description
+ * @n: variable
+ * Return: 1, 0, '-1'
+ */
+
+int print_sign(int n)
+{
+	if (n > 0)
+        {
+                _putchar('+');
+                return (1);
+        }
+        if (n < 0)
+        {
+                _putchar('-');
+                return (-1);
+        }
+        if (n == 0)
+        {
+                _putchar('0');
+                return (0);
+        }
+	else
+	{
+		return (0);
+	}
+}
