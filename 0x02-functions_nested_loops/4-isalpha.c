b9df2b5aff189024b490c642692924984ee7ac9d
Ajayi <ajayidavies7@gmail.com>
2022-04-13 07:38:16 -0700

    4

---
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+ * _isalpha - Short description, check alphabet
+ * @c: variable value
+ * Return: on success (0)
+ */
+
+int _isalpha(int c)
+{
+	if (c >= 'A' && c <= 'Z')
+	{
+		return (1);
+	}
+	if (c >= 'a' && c <= 'z')
+	{
+		return (1);
+	}
+	else
+	{
+		return (0);
+	}
+}
+
