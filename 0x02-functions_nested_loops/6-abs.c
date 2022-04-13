a733685ad0d1fdc97be4febd401ed85ac4230777
Ajayi <ajayidavies7@gmail.com>
2022-04-13 12:32:45 -0700

    6

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * _abs - Short description, reverse signs
+ * @r: interger variable
+ *
+ * Return: Always 0 (Success)
+ */
+
+int _abs(int r)
+{
+	if (r < 0)
+	{
+		return (-1 * r);
+	}
+	else
+		return (r);
+
+}
---
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,12 @@
+#inlclude "main.h"
+
+/**
+ * print_last - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+
+int print_last_digit(int)
+{
+
+}
