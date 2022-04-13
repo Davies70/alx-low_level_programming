b824d244c0a41a14136599a58745d72a7869676e
Ajayi <ajayidavies7@gmail.com>
2022-04-12 19:46:34 -0700

    1

--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -1,5 +1,5 @@
-#ifndef MAIN_H
-#define MAIN_H
+#ifndef _MAIN_H_
+#define _MAIN_H_
 
 int _putchar(char c);
 void print_alphabet(void);
@@ -15,4 +15,5 @@ int add(int, int);
 void print_to_98(int n);
 void print_times_table(int n);
 
-#endif 
+#endif
+
