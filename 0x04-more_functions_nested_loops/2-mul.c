202e885ebb46910a3a09761b8bc86032d536a197
Ajayi <ajayidavies7@gmail.com>
2022-04-15 08:27:32 -0700

    2

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * mul - Entry point
+ *@a: interger variable
+ *@b: interger varible
+ * Return: c
+ */
+int mul(int a, int b)
+{
+	int c;
+
+	c = a * b;
+	return (c);
+}
+
+
