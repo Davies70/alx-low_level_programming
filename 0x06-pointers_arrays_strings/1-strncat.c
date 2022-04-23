9bd8424cd5ae7f94fe451daa0aa59d03bcd9e84d
Ajayi <ajayidavies7@gmail.com>
2022-04-23 14:25:33 -0700

    1

---
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -0,0 +1,28 @@
+#include "main.h"
+
+/**
+ * _strncat - Entry point
+ *@dest: first pointer
+ *@src: second pointer
+ *@n: byte limit
+ * Return: Always 0 (Success)
+ */
+
+char *_strncat(char *dest, char *src, int n)
+{
+	int i = 0;
+	int j = 0;
+
+	while (dest[i] != '\0')
+		i++;
+	while (src[j])
+	{
+		if (j < n)
+		{
+			dest[i] = src[j];
+			i++;
+			j++;
+		}
+	}
+	return (dest);
+}
