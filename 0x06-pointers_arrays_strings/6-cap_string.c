9a897271a8a6dff90f20922e27f522ab8ae5c448
Ajayi <ajayidavies7@gmail.com>
2022-04-24 08:18:38 -0700

    6

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -1,39 +1,40 @@
 #include<stdio.h>
 
 /**
- * upper - a function ...
- * @c: the caractere
- *
- * Return: 1 or 0.
- */
+* upper - a function ...
+* @c: the caractere
+*
+* Return: 1 or 0.
+*/
 
 char	upper(char c)
 {
-	char	car;
+char	car;
 
-	if (c >= 'a' && c <= 'z')
-		car = c + 'A' - 'a';
-	else
-		car = c;
-	return (car);
+if (c >= 'a' && c <= 'z')
+car = c + 'A' - 'a';
+else
+car = c;
+return (car);
 }
 
 /**
- * cap_string - a function ...
- * @str: the chaine of caractere
- *
- * Return: str
- */
+* cap_string - a function ...
+* @str: the chaine of caractere
+*
+* Return: str
+*/
 
 char	*cap_string(char *str)
 {
-	int	i;
+int	i;
 
-	i = 0;
-	while (str[i])
-	{
-		str[i] = upper(str[i]);
-		i++;
-	}
-	return (str);
+i = 0;
+while (str[i])
+{
+str[i] = upper(str[i]);
+i++;
 }
+return (str);
+}
+
