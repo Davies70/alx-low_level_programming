4e5682922fb0612e6d51b82f0ad69953389ad082
Ajayi <ajayidavies7@gmail.com>
2022-04-26 14:15:01 -0700

    0

---
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/**
+ * _memset - funtion to replace memory block with constant character
+ *@s: string name
+ *@b: const. char.
+ *@n: char. size
+ * Return: s
+ */
+
+char *_memset(char *s, char b, unsigned int n)
+{
+	while (n > 0)
+	{
+		*s = b;
+		s++;
+		n--;
+	}
+	return (s);
+}
