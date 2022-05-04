b75db45cb5af93e33cce8eb7d4e2bca85134fb35
Ajayi <ajayidavies7@gmail.com>
2022-05-04 14:47:44 -0700

    1

---
+++ 0x0B-malloc_free/1-strdup.c
@@ -0,0 +1,45 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "main.h"
+/**
+ * _strdup - function to copy string into ram
+ *@str: string
+ * Return: p
+ */
+
+char *_strdup(char *str)
+{
+	int i;
+	char *p;
+	int n;
+
+	if (str == NULL)
+		return (NULL);
+	n = _len(str);
+	p = malloc(sizeof(char) * (n + 1));
+	for (i = 0; i < (n + 1); i++)
+	{
+		p[i] = str[i];
+	}
+	return (p);
+}
+
+/**
+ * _len - function to return string length
+ *@str: string
+ * Return: i
+ */
+int _len(char *str)
+{
+	int i;
+
+	while (str[i] != '\n')
+	{
+		i++;
+	}
+	return (i);
+}
+
+
+
+
