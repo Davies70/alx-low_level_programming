d683fd74d0e2f216109bb2cdac1965c2a21aa4ce
Ajayi <ajayidavies7@gmail.com>
2022-05-04 12:23:03 -0700

    1

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -17,9 +17,10 @@ char *create_array(unsigned int size, char c)
 	if (size == 0)
 		return (NULL);
 	b = malloc(sizeof(char) * size);
-	for (i = 0; i < size; i++)
+	if (b != NULL)
 	{
-		b[i] = c;
+		for (i = 0; i < size; i++)
+			b[i] = c;
 	}
 	return (b);
 }
