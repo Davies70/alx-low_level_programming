a37b06dbc2a30e0472c98f01eec064ab7c84cdf7
Ajayi <ajayidavies7@gmail.com>
2022-05-10 11:42:59 -0700

    1

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -2,17 +2,18 @@
 #define DOG_H
 #include <stdio.h>
 /**
- * struct dog - struct declaration
- * @name: string
- * @age: float variable
- * @owner: string
- */
+* struct dog - struct declaration
+* @name: string
+* @age: float variable
+* @owner: string
+*/
 typedef struct dog
 {
 char *name;
 float age;
 char *owner;
-}dog_t;
+}
+dog_t;
 void init_dog(struct dog *d, char *name, float age, char *owner);
 void print_dog(struct dog *d);
 #endif
