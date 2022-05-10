822fb098038cbbb6607a7827d16624e7139d7dfd
Ajayi <ajayidavies7@gmail.com>
2022-05-10 11:34:02 -0700

    1

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -7,12 +7,12 @@
  * @age: float variable
  * @owner: string
  */
-struct dog
+typedef struct dog_t
 {
 char *name;
 float age;
 char *owner;
-};
+}dog_t;
 void init_dog(struct dog *d, char *name, float age, char *owner);
 void print_dog(struct dog *d);
 #endif
