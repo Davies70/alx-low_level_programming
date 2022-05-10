49e70209d270bddafb848f22dcc71374bb0ce9f3
Ajayi <ajayidavies7@gmail.com>
2022-05-10 05:16:18 -0700

    1

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,16 @@
+#ifndef DOG_H
+#define DOG_H
+#include <stdio.h>
+/**
+ * struct dog - struct declaration
+ * @name: string
+ * @age: float variable
+ * @owner: string
+ */
+struct dog
+{
+char *name;
+float age;
+char *owner;
+}
+#endif
