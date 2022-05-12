82d646c4a679b8c47a6fba1290b11fe49383b715
Ajayi <ajayidavies7@gmail.com>
2022-05-12 13:21:56 -0700

    0

---
+++ 0x10-variadic_functions/0-sum_them_all.c
@@ -0,0 +1,21 @@
+#include <stdarg>
+#include <stdio.h>
+/**
+* sum_them_all - func to sum
+* @n: arg
+* Return: sum
+*/
+int sum_them_all(const unsigned int n, ...)
+{
+int i = 0, sum = 0;
+
+if (n == 0)
+return (0);
+va_list ap;
+va_start(ap, n);
+for (i = 0; i < n; i++)
+sum += va_arg(ap, const unsigned int);
+va_end(ap);
+return (sum);
+}
+
