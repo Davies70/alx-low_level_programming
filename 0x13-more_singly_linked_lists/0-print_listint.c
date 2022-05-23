eaa44f5ef72508f6a011fbcc29affb7f622d3592
Ajayi <ajayidavies7@gmail.com>
2022-05-23 10:25:07 -0700

    1

---
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -0,0 +1,24 @@
+#include "lists.h"
+#include <stdio.h>
+
+/**
+*print_listint - print elements of list
+*@h: head node
+*
+*/
+size_t print_listint(const listint_t *h)
+{
+int count = 0;
+const listint_t *current;
+
+if (h == NULL)
+return (0);
+current = h;
+while (current != NULL)
+{
+printf("%d\n", current->n);
+current = current->next;
+count++;
+}
+return (count);
+}
