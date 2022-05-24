2a8f65388c008e580e4f2dbda7380cbd22d39126
Ajayi <ajayidavies7@gmail.com>
2022-05-24 09:26:20 -0700

    3

--- 0x13-more_singly_linked_lists/6-pop_listint.c
+++ 0x13-more_singly_linked_lists/6-pop_listint.c
@@ -7,12 +7,14 @@
 */
 int pop_listint(listint_t **head)
 {
-listint_t *current;
+listint_t *temp;
+int number;
 
 if (*head = NULL || head == NULL)
 return (0);
-current = *head;
-*head = (*head)->next;
-free(current);
-return (current->n):
+number = (*head)->n;
+temp = (*head)->next;
+free(head);
+*head = temp;
+return (number):
 }
