e4f87d9c45a453c83e288512007f2dd01a1ddc98
Ajayi <ajayidavies7@gmail.com>
2022-05-24 05:48:58 -0700

    1

---
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -0,0 +1,19 @@
+#include "lists.h"
+
+/**
+* free_listint2 - free list
+* @head: head node
+*/
+
+void free_listint2(listint_t **head)
+{
+listint_t *current = *head;
+listint_t *temp;
+
+while (current != NULL)
+{
+temp = current;
+current = current->next;
+free (temp);
+}
+}
