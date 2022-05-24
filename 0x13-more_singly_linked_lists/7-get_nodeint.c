dd1d4e7dbeec3fdbbd227ba90184c821f8a90db2
Ajayi <ajayidavies7@gmail.com>
2022-05-24 12:27:57 -0700

    currrent node

---
+++ 0x13-more_singly_linked_lists/7-get_nodeint.c
@@ -0,0 +1,24 @@
+#include "lists.h"
+
+/**
+* get_nodeint_at_index - return the node of the lists
+* @head: head pointer
+* @index: index of node
+* Return: node
+*/
+listint_t *get_nodeint_at_index(listint_t *head, unsigned int index)
+{
+listint_t *current, *node;
+unsigned int count = 0;
+
+current = head;
+if (!head)
+return (NULL);
+while (count < index)
+{
+current = current->next;
+count++;
+}
+node = current;
+return (node);
+}
