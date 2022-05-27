0a0e7ecf36eebebd35cc5fc2f937b027e152e2b3
Ajayi <ajayidavies7@gmail.com>
2022-05-27 06:20:45 -0700

    4

---
+++ 0x14-bit_manipulation/2-get_bit.c
@@ -0,0 +1,24 @@
+#include "main.h"
+/**
+* get_bit - return bit at given pos
+* @n: decimal
+* @index: position
+* Return: value
+*/
+
+int get_bit(unsigned long int n, unsigned int index)
+{
+int value;
+
+if (index == 0)
+{
+value = n & 1;
+return (value);
+}
+else
+{
+value = (n >> index) & 1;
+return (value);
+}
+return (-1);
+}
