a1de258474b594975a8a137cfeb5de4379167b60
Ajayi <ajayidavies7@gmail.com>
2022-05-27 09:20:53 -0700

    set bit

--- 0x14-bit_manipulation/3-set_bit.c
+++ 0x14-bit_manipulation/3-set_bit.c
@@ -1,14 +1,14 @@
 #include "main.h"
 /**
- * set_bit - func that sets new bit
- * @n: decimal int
- * @index: position
- * Return: 1 or -1
- */ 
+* set_bit - func that sets new bit
+* @n: decimal int
+* @index: position
+* Return: 1 or -1
+*/ 
 int set_bit(unsigned long int *n, unsigned int index)
 {
-	if (index > sizeof(n) * 8 - 1)
-		return (-1);
-	*n |= 1UL << index;
-	return (1);
+if (index > sizeof(n) * 8 - 1)
+return (-1);
+*n |= 1UL << index;
+return (1);
 }
