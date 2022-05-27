b23327d8fbb3272eae923401eec6788a07afec73
Ajayi <ajayidavies7@gmail.com>
2022-05-27 12:44:43 -0700

    1

--- 0x14-bit_manipulation/5-flip_bits.c
+++ 0x14-bit_manipulation/5-flip_bits.c
@@ -10,6 +10,10 @@ unsigned int flip_bits(unsigned long int n, unsigned long int m)
 int value;
 int count = 0;
 
+if (n > ULONG_MAX)
+return (-1);
+if (m > ULONG_MAX)
+return (-1);
 value = n ^ m;
 while (value > 0)
 {
