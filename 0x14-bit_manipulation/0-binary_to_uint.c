bcf93765ebe7e5626ea6e0bf9fd1b5311b448f42
Ajayi <ajayidavies7@gmail.com>
2022-05-26 18:27:40 -0700

    2

---
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -0,0 +1,32 @@
+#include "main.h"
+
+
+/**
+* binary_to_uint - function that
+* coverts binary to decimals
+* @b: pointer to string of zeros and ones
+* Return: 0 or converted number
+*/
+
+unsigned int binary_to_uint(const char *b)
+{
+unsigned int decimal = 0, weight = 1;
+int index;
+int i = 0;
+
+while (b[i] != '\0')
+{
+i++;
+}
+for (index = i - 1; index >= 0; index--)
+{
+if (b[index] != '1' && b[index] != '0')
+return (0);
+if (b[index] == '1')
+{
+decimal += weight;
+}
+weight *= 2;
+}
+return (decimal);
+}
