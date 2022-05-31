3c2a0c37d54e75a7752c440024ffc8b260ec8ad9
Ajayi <ajayidavies7@gmail.com>
2022-05-31 12:55:10 -0700

    3

--- 0x15-file_io/main.h
+++ 0x15-file_io/main.h
@@ -8,7 +8,7 @@
 #include <sys/stat.h>
 #include <fcntl.h>
 
-#define BUFFER 9000
+#define BUFSIZE 9000
 
 ssize_t read_textfile(const char *filename, size_t letters);
 int create_file(const char *filename, char *text_content);
