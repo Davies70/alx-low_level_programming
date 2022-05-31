36149b2ae8e35d6235550d9e90a20fb9f9517fd0
Ajayi <ajayidavies7@gmail.com>
2022-05-31 12:09:39 -0700

    0

---
+++ 0x15-file_io/0-read_textfile.c
@@ -0,0 +1,24 @@
+#include "main.h"
+/**
+ * read_textfile - function to read file
+ * @filename: name of file to be read
+ * @letters: size of characters to be read
+ * Return: size of letters it could read
+ */
+ssize_t read_textfile(const char *filename, size_t letters)
+{
+	int fd, size;
+	char portion[BUFFER + 1];
+
+	if (filename == NULL)
+		return (0);
+	fd = open(filename, O_RDONLY);
+	if (fd == -1)
+		return (0);
+	size = read(fd, portion, letters);
+	portion[size] = '\0';
+	printf("%s", portion);
+	close(fd);
+	return (size);
+}
+
