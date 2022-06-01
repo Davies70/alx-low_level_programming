5db70550bbdaab8813c695e1dc6f1ce9de5b20ee
Ajayi <ajayidavies7@gmail.com>
2022-06-01 16:03:15 -0700

    test

--- 0x15-file_io/main.h
+++ 0x15-file_io/main.h
@@ -9,7 +9,7 @@
 #include <sys/stat.h>
 #include <fcntl.h>
 
-#define BUFSIZE 9000
+#define BUFSIZE 1024
 
 ssize_t read_textfile(const char *filename, size_t letters);
 int create_file(const char *filename, char *text_content);
