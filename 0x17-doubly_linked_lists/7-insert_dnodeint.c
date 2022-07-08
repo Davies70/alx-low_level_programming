5f0e31d1eede6574d0e15c4e72339cbcc8f749bd
Ajayi <ajayidavies7@gmail.com>
2022-07-08 11:57:44 -0700

    7

--- 0x17-doubly_linked_lists/7-insert_dnodeint.c
+++ 0x17-doubly_linked_lists/7-insert_dnodeint.c
@@ -15,7 +15,7 @@ dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 	unsigned int size = 0;
 	unsigned int i = 0;
 
-	if (h* == NULL)
+	if (*h == NULL)
 		return NULL;
 	count = *h;
 	while (count->next != NULL)
